09957a72b4fc4c7c50a511be21b44f535bef94c1
aayur05 <104608429+aayur05@users.noreply.github.com>
2024-03-02 13:06:23 +0545

    Add files via upload

---
+++ Client/src/modal/modal2.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
+const UpdateModal = ({ isOpen, onClose, onUpdate }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [role, setRole] = useState('');
+
+  const handleUpdate = () => {
+    const updatedUserData = {
+      name,
+      email,
+      username,
+      phoneNumber,
+      role,
+    };
+    onUpdate(updatedUserData);
+    onClose();
+  };
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      contentLabel="Update User Modal"
+    >
+      <h2>Update User Data</h2>
+      <form>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Phone Number:
+          <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Role:
+          <input type="text" value={role} onChange={(e) => setRole(e.target.value)} />
+        </label>
+        <br />
+        <button type="button" onClick={handleUpdate}>
+          Update
+        </button>
+      </form>
+    </Modal>
+  );
+};
+
+export default UpdateModal;
