fd0f003c7e71ffa20abefb3afe813ca09a2851e8
legendofzer0 <98453046+legendofzer0@users.noreply.github.com>
2024-06-26 08:20:17 +0545

    dashboard

--- Client/src/components/AdminNavbar.jsx
+++ Client/src/components/AdminNavbar.jsx
@@ -3,37 +3,32 @@ import "../css/root.css";
 
 function AdminNavbar() {
   return (
-    <>
-      <nav>
-        <Link to="/" className="nav-element">
-          Home
-        </Link>
-        <span className="hover-element">
-          Dashboard
-          <span className="hide">
-            <Link to="/CreateUser" className="nav-element">
-              Create User
-            </Link>
-            <br />
-            <Link to="/CreateItem" className="nav-element">
-              Create Item
-            </Link>
-            <br />
-            <Link to="/ListUser" className="nav-element">
-              List User
-            </Link>
-            <br />
-            <Link to="/ListItem" className="nav-element">
-              List Item
-            </Link>
-            <br />
-          </span>
-        </span>
-        <Link to="/MyAccount" className="nav-element">
-          MyAccount
-        </Link>
-      </nav>
-    </>
+    <nav>
+      <Link to="/" className="nav-element">
+        Home
+      </Link>
+      <div className="nav-element dropdown">
+        Dashboard
+        <div className="dropdown-content">
+          <Link to="/CreateUser" className="dropdown-item">
+            Create User
+          </Link>
+          <Link to="/CreateItem" className="dropdown-item">
+            Create Item
+          </Link>
+          <Link to="/ListUser" className="dropdown-item">
+            List User
+          </Link>
+          <Link to="/ListItem" className="dropdown-item">
+            List Item
+          </Link>
+        </div>
+      </div>
+      <Link to="/MyAccount" className="nav-element">
+        MyAccount
+      </Link>
+    </nav>
   );
 }
+
 export default AdminNavbar;
