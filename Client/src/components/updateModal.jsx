4e979ef1faab45f7945168e87ec6ee79f337163d
legendofzer0 <98453046+legendofzer0@users.noreply.github.com>
2024-07-21 10:26:37 +0545

    Revert "#updateBugFix"
    This reverts commit 709cb3ffdab9666573345a4adbdd00b90ada088b.

--- Client/src/components/updateModal.jsx
+++ Client/src/components/updateModal.jsx
@@ -1,27 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import "../css/modal.css";
 
 const UpdateUserModal = ({ user }) => {
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
+  console.log(user);
+  const [fullName, setFullName] = useState(user.username);
+  const [email, setEmail] = useState(user.email);
+  const [username, setUsername] = useState(user.username);
+  const [phoneNumber, setPhoneNumber] = useState(user.phone_number);
   const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
   const isValidEmail = /\S+@\S+\.\S+/;
-
-  useEffect(() => {
-    setFullName(user.full_name);
-    setEmail(user.email);
-    setUsername(user.username);
-    setPhoneNumber(user.phone_number);
-  }, [user]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setError("");
-    setSuccess("");
 
     if (!username || !email || !phoneNumber || !fullName) {
       setError("Please fill all fields.");
@@ -37,15 +29,14 @@ const UpdateUserModal = ({ user }) => {
     }
 
     try {
-      // console.log(username, fullName, email, phoneNumber, user);
-      const response = await axios.put(`/api/user/${user.user_id}`, {
+      const response = await axios.put(`/api/user/${user.Id}`, {
         username,
         email,
         phone_number: phoneNumber,
         full_name: fullName,
       });
+      // Handle response or close modal here
       console.log(response);
-      setSuccess("User updated successfully.");
       window.location.reload(false);
     } catch (error) {
       console.error("Error updating user:", error);
@@ -56,53 +47,52 @@ const UpdateUserModal = ({ user }) => {
   return (
     <div className="center middle">
       <div className="width">
-        <div className="modal form">
-          <form onSubmit={handleSubmit}>
-            <h2>Update User</h2>
-            {error && <div className="error-message">{error}</div>}
-            {success && <div className="success-message">{success}</div>}
+      <div className="modal form">
+        <form onSubmit={handleSubmit}>
+          <h2>Update User</h2>
+          {error && <div className="error-message">{error}</div>}
 
-            <label>Name:</label>
-            <br />
-            <input
-              type="text"
-              value={fullName}
-              onChange={(e) => setFullName(e.target.value)}
-              required
-            />
-            <br />
-            <label>Email:</label>
-            <br />
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <br />
-            <label>Username:</label>
-            <br />
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-            <br />
-            <label>Phone Number:</label>
-            <br />
-            <input
-              type="text" // Changed to text to handle leading zeros
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-              required
-            />
-            <br />
-            <button type="submit" className="submit">
-              Update
-            </button>
-          </form>
-        </div>
+          <label>Name:</label>
+          <br />
+          <input
+            type="text"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            required
+          />
+          <br />
+          <label>Email:</label>
+          <br />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <br />
+          <label>Username:</label>
+          <br />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <br />
+          <label>Phone Number:</label>
+          <br />
+          <input
+            type="text" // Changed to text to handle leading zeros
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            required
+          />
+          <br />
+          <button type="submit" className="submit">
+            Update
+          </button>
+        </form>
+      </div>
       </div>
     </div>
   );
